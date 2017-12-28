316d23aa3786025974034f815e17a27527dd6570
gomenjukmarija <gomenjukmarija@gmail.com>
2017-12-28 14:47:44 +0200

    Keys

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// If you want to create a list of JSX elements, then .map() is often your best bet.
-
 const people = ['Rowe', 'Prevost', 'Gare'];
 
-const peopleLis = people.map(person =>
-    <li>{person}</li>
+const peopleLis = people.map((person, i) =>
+    // expression goes here:
+    <li key={'person_' + i}>{person + i}</li>
 );
 
-ReactDOM.render(
-    <ul>{peopleLis}</ul>,
-    document.getElementById('app')
-);
+ReactDOM.render(<ul>{peopleLis}</ul>,
+document.getElementById('app'));

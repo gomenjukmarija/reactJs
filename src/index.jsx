50f64c9276ac627fde6269baaace2a298a25c320
gomenjukmarija <gomenjukmarija@gmail.com>
2017-12-28 13:30:38 +0200

    Use this in a Component

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class TodaysPlan extends React.Component {
+class MyName extends React.Component {
+
+    get name() {
+        return 'whatever-your-name-is-goes-here';
+    }
+
+    // what does this mean?
+    // The simple answer is that this refers to an instance of MyName.
+    // The less simple answer is that this refers to the object on which this's enclosing method,
+    // in this case .render(), is called.
+    // It is almost inevitable that this object will be an instance of MyName, but technically it could be something else.
 
     render() {
-
-        const apocalypse = Math.random() < 0.5;
-
-        let task;
-        if (!apocalypse) {
-            task = 'learn React.js'
-        } else {
-            task = 'run around'
-        }
-
-        return <h1>Today I am going to {task}!</h1>;
+        return <h1>My name is {this.name}!</h1>;
     }
 }
 
-ReactDOM.render(
-    <TodaysPlan />,
-    document.getElementById('app')
-);
+ReactDOM.render(<MyName />, document.getElementById('app'));

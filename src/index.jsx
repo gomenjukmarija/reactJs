88a1ebe809028234e28689526c291d7e38eb4dea
gomenjukmarija <gomenjukmarija@gmail.com>
2017-12-28 11:58:09 +0200

    Rendering JSX, ReactDOM

--- src/index.jsx
+++ src/index.jsx
@@ -1,58 +1,38 @@
-// This JSX element looks exactly like HTML!
-// The only noticeable difference is that you would find it in a JavaScript file,
-// instead of in an HTML file.
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const navBar = <nav>I am a nav bar</nav>;
+//ReactDOM is the name of a JavaScript library.
+// ReactDOM.render()'s first argument should evaluate to a JSX expression,
+// it doesn't have to literally be a JSX expression.//
+// The first argument could also be a variable, so long as that variable evaluates to a JSX expression.
 
-const myTeam = {
-    center: <li>Benzo Walli</li>,
-    powerForward: <li>Rasha Loa</li>,
-    smallForward: <li>Tayshaun Dasmoto</li>,
-    shootingGuard: <li>Colmar Cumberbatch</li>,
-    pointGuard: <li>Femi Billon</li>
-};
+// The second argument document.getElementById('app').//
+//That element acted as a container for ReactDOM.render()'s first argument!
+// At the end of the previous exercise, this appeared on the screen:
+// <main id="app">
+//     <h1>Render me!</h1>
+// </main>
 
-//Here are some JSX elements with attributes:
-
-<a href="http://www.example.com">Welcome to the Web</a>;
-const title = <h1 id="title">Introduction to React.js: Part I</h1>;
-
-// A single JSX element can have many attributes, just like in HTML:
-
-const panda = <img src="images/panda.jpg" alt="panda" width="500px" height="500px" />;
-
-
-// Here's an example of a JSX <h1> element, nested inside of a JSX <a> element:
-// If a JSX expression takes up more than one line,
-// then you must wrap the multi-line JSX expression in parentheses.
-
-    const theExample = (
-    <a href="https://www.example.com">
-        <h1>
-            Click me!
-        </h1>
-    </a>
+const toDoList = (
+    <ol>
+        <li>Learn React</li>
+        <li>Become a Developer</li>
+    </ol>
 );
 
-// There's a rule that we haven't mentioned:
-// a JSX expression must have exactly one outermost element.
-
-
-// In other words, this code will work:
-
-const paragraphs = (
-    <div id="i-am-the-outermost-element">
-        <p>I am a paragraph.</p>
-        <p>I, too, am a paragraph.</p>
-    </div>
+ReactDOM.render(
+    toDoList,
+    document.getElementById('app')
 );
 
-//But this code will not work:
 
-const paragraphs = (
-    <p>I am a paragraph.</p>
-    <p>I, too, am a paragraph.</p>
+// One special thing about ReactDOM.render() is that it only updates DOM elements that have changed.
+// That means that if you render the exact same thing twice in a row, the second render will do nothing:
+
+ReactDOM.render(
+    toDoList,
+    document.getElementById('app')
 );
 
-// If you notice that a JSX expression has multiple outer elements,
-// the solution is usually simple: wrap the JSX expression in a <div></div>.
+//Only updating the necessary DOM elements is a large part of what makes React so successful.
+

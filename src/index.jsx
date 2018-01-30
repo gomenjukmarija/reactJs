c75d61e646d5647fc99322dbd941c5c424fc1836
gomenjukmarija <gomenjukmarija@gmail.com>
2018-01-30 11:57:58 +0200

    Access a Component's state

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// React components will often need dynamic information in order to render.
-// For example, imagine a component that displays the score of a basketball game.
-// The score of the game might change over time, meaning that the score is dynamic.
-// Our component will have to know the score, a piece of dynamic information, in order to render in a useful way.
-
-// There are two ways for a component to get dynamic information: props and state.
-// Besides props and state, every value used in a component should always stay exactly the same.
-
-// Unlike props, a component's state is not passed in from the outside. A component decides its own state.
-
 class App extends React.Component {
     // constructor method begins here:
     constructor(props) {
@@ -21,8 +11,13 @@ class App extends React.Component {
     render() {
         return (
             <h1>
-                Wow this entire app is just an h1.
+                {this.state.title}
             </h1>
         );
     }
 }
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);

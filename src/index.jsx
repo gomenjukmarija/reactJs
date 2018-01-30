59e19487f70084658775944975f5967d413bc588
gomenjukmarija <gomenjukmarija@gmail.com>
2018-01-30 12:27:57 +0200

    Call this.setState from Another Function

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-    // constructor method begins here:
-    constructor(props) {
+const green = '#39D1B4';
+const yellow = '#FFD712';
+
+class Toggle extends React.Component {
+    constructor(props){
         super(props);
-        this.state = { title: 'Best App' };
+        this.state = { color: green };
+        this.changeColor = this.changeColor.bind(this);
+    }
+
+    changeColor() {
+        const newColor = this.state.color == green ? yellow : green;
+        this.setState({ color: newColor });
     }
 
     render() {
         return (
-            <h1>
-                {this.state.title}
-            </h1>
+            <div style={{background: this.state.color}}>
+                <h1>
+                    Change my color
+                </h1>
+                <button onClick={this.changeColor}>
+                    Change color
+                </button>
+            </div>
         );
     }
 }
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('app')
-);
+ReactDOM.render(<Toggle />, document.getElementById('app'));

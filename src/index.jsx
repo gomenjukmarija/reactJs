9e75bf943a68ef5c1b2cd9a9128a71e3df041b39
gomenjukmarija <gomenjukmarija@gmail.com>
2018-01-30 11:28:41 +0200

    handleEvent, onEvent, and this.props.onEvent

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,28 @@ import ReactDOM from 'react-dom';
 
 import { Button } from './child.jsx';
 
+// Here's how the naming convention works: first, think about what type of event you are listening for.
+// In our example, the event type was "click."
+// If you are listening for a "click" event, then you name your event handler handleClick.
+// If you are listening for a "keyPress" event, then you name your event handler handleKeyPress:
+
+// Your prop name should be the word on, plus your event type.
+// If you are listening for a "click" event, then you name your prop onClick.
+// If you are listening for a "keyPress" event, then you name your prop onKeyPress:
+
+// class MyClass extends React.Component {
+//     handleHover() {
+//         alert('I am an event handler.');
+//         alert('I will listen for a "hover" event.');
+//     }
+//
+//     render() {
+//         return <Child onHover={this.handleHover} />;
+//     }
+// }
+
 class Talker extends React.Component {
-    talk() {
+    handleClick() {
         let speech = '';
         for (let i = 0; i < 10000; i++) {
             speech += 'blah ';
@@ -13,7 +33,13 @@ class Talker extends React.Component {
     }
 
     render() {
-        return <Button talk={this.talk} />;
+
+        // The name onClick does not create an event listener when used on <Button />
+        // - it's just an arbitrary attribute name:
+        // The reason for this is that in <Button /> is not an HTML-like JSX element; it's a component instance.
+        // Names like onClick only create event listeners if they're used on HTML-like JSX elements.
+        // Otherwise, they're just ordinary prop names.
+        return <Button onClick={this.handleClick}/>;
     }
 }
 

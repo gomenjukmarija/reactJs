bf488ccc652c9da3d7260fa23e0cbaa3bb0095a1
gomenjukmarija <gomenjukmarija@gmail.com>
2018-01-30 12:45:11 +0200

    Pass a Component's State

--- src/child.jsx
+++ src/child.jsx
@@ -1,16 +1,7 @@
 import React from 'react';
 
-export class List extends React.Component {
+export class Child extends React.Component {
     render() {
-        let titleText = `Favorite ${this.props.type}`;
-        if (this.props.children instanceof Array) {
-            titleText += 's';
-        }
-        return (
-            <div>
-                <h1>{titleText}</h1>
-                <ul>{this.props.children}</ul>
-            </div>
-        );
+        return <h1>Hey, my name is {this.props.name}!</h1>;
     }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Child } from './child.jsx';
 
-// Our programming pattern uses two React components: a stateful component, and a stateless component.
-// "Stateful" describes any component that has a state property;
-// "stateless" describes any component that does not.
-// In our pattern, a stateful component passes its state down to a stateless component.
-
-// Let's make a stateful component pass its state to a stateless component.
-//
-// To make that happen, you need two component classes: a stateful class, and a stateless class.
 
 class Parent extends React.Component {
     constructor(props) {
@@ -17,6 +10,11 @@ class Parent extends React.Component {
     }
 
     render() {
-        return <div></div>;
+        return <Child name={this.state.name} />;
     }
 }
+
+ReactDOM.render(
+    <Parent />,
+    document.getElementById('app')
+);

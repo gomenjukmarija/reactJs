34ad05e9ff2abb8aa695172720e7e0c22d31cd2a
gomenjukmarija <gomenjukmarija@gmail.com>
2018-01-30 11:48:07 +0200

    defaultProps

--- src/index.jsx
+++ src/index.jsx
@@ -1,59 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { List } from './child.jsx';
-
-// Every component's props object has a property named children.
-
-// this.props.children will return everything in between a component's opening and closing JSX tags.
-
-// So far, all of the components that you've seen have been self-closing tags, such as <MyComponentClass />.
-// They don't have to be! You could write <MyComponentClass></MyComponentClass>, and it would still work.
-
-// this.props.children would return everything in between <MyComponentClass> and </MyComponentClass>.
-
-// In Example 1, <BigButton>'s this.props.children would equal the text, "I am a child of BigButton."
-//
-//  In Example 2, <BigButton>'s this.props.children would equal a <LilButton /> component.
-//
-// In Example 3, <BigButton>'s this.props.children would equal undefined.
-
-//  Example 1
-// <BigButton>
-//     I am a child of BigButton.
-// </BigButton>
-//
-//
-//  Example 2
-// <BigButton>
-// <LilButton />
-// </BigButton>
-//
-//
-// Example 3
-// <BigButton />
-
-// If a component has more than one child between its JSX tags, then this.props.children will return those children in an array.
-// However, if a component has only one child, then this.props.children will return the single child, not wrapped in an array.
-
-class App extends React.Component {
+class Button extends React.Component {
     render() {
         return (
-            <div>
-                <List type='Living Musician'>
-                    <li>Sachiko M</li>
-                    <li>Harvey Sid Fisher</li>
-                </List>
-                <List type='Living Cat Musician'>
-                    <li>Nora the Piano Cat</li>
-                </List>
-            </div>
+            <button>
+                {this.props.text}
+            </button>
         );
     }
 }
 
+// If nobody passes any text to Button, then Button's display will be blank.
+// It would be better if Button could display a default message instead.
+Button.defaultProps = { text: 'I am a button' };
+
+// Your new prop should override the default, making the <button></button> sad again :(
 ReactDOM.render(
-    <App />,
+    <Button text=""/>,
     document.getElementById('app')
 );
-

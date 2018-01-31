05c4adfb79f48381bcf166d57432b94f644085f4
gomenjukmarija <gomenjukmarija@gmail.com>
2018-01-31 10:00:38 +0200

    Define an Event Handler

--- src/child.jsx
+++ src/child.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
 
-// 4) The child receives the passed-down function, and uses it as an event handler.
-// When a user clicks on the <button></button>,
-// a click event will fire.
-// This will make the passed-down function get called,
-// which will update the parent's state.
+// 1) To make a child component update its parent's state,
+// the first step is something that you've seen before:
+// you must define a state-changing method on the parent.
 
-export class ChildClass extends React.Component {
+// Notice that the name inside of the <h1></h1> is equal to this.props.name.
+// In order to make the dropdown menu change the <h1></h1>,
+// you will need the dropdown menu to change the value of this.props.name!
+
+export class Child extends React.Component {
     render() {
         return (
-            // The stateless component class uses
-            // the passed-down handleClick function,
-            // accessed here as this.props.onClick,
-            // as an event handler:
-            <button onClick={this.props.onClick}>
-                Click Me!
-            </button>
+            <div>
+                <h1>
+                    Hey my name is {this.props.name}!
+                </h1>
+                <select id="great-names">
+                    <option value="Frarthur">
+                        Frarthur
+                    </option>
+
+                    <option value="Gromulus">
+                        Gromulus
+                    </option>
+
+                    <option value="Thinkpiece">
+                        Thinkpiece
+                    </option>
+                </select>
+            </div>
         );
     }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,45 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ChildClass } from './child.jsx';
+import { Child } from './child.jsx';
 
-// The stateless, child component will update the state of the parent component.
+// Parent renders a <Child />, passing in a name prop.
+// This name prop is the same value that the <Child /> displays in its <h1></h1>.
+// You need Child's dropdown menu to change Parent's this.state.name!
+// That will cause <Child /> to get passed a new name prop,
+// which will change the name displayed on the screen.
 
-// 1) The parent component class defines a method that calls this.setState().
-// For an example, look  at the .handleClick() method.
-
-// 2) The parent component binds the newly-defined method to the current instance of the component in its constructor.
-// This ensures that when we pass the method to the child component, it will still update the parent component.
-//  For an example, look  at the end of the constructor() method this.handleClick = this.handleClick.bind(this)
-
-// 3) Once the parent has defined a method that updates its state and bound to it,
-// the parent then passes that method down to a child.//
-// Look  at the prop <ChildClass onClick={this.handleClick} />
-
-
-class ParentClass extends React.Component {
+class Parent extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { totalClicks: 0 };
-
-        this.handleClick = this.handleClick.bind(this);
+        this.state = { name: 'Frarthur' };
     }
 
-    handleClick() {
-        const total = this.state.totalClicks;
-
-        // calling handleClick will
-        // result in a state change:
-        this.setState(
-            { totalClicks: total + 1 }
-        );
+    changeName(newName) {
+        this.setState({
+            name: newName
+        });
     }
 
-    // The stateful component class passes down
-    // handleClick to a stateless component class:
     render() {
-        return (
-            <ChildClass onClick={this.handleClick} />
-        );
+        return <Child name={this.state.name} />
     }
 }
+
+ReactDOM.render(
+    <Parent />,
+    document.getElementById('app')
+);

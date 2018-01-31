06b1aad7c43a840aa62eb3c1aebbbc2ab7ce82d4
gomenjukmarija <gomenjukmarija@gmail.com>
2018-01-31 11:52:56 +0200

    Share Styles Across Multiple Components

--- src/child.jsx
+++ src/child.jsx
@@ -1,46 +1,16 @@
 import React from 'react';
+import { styles } from './styles.jsx';
 
-// A child component updates its parent's state,
-// and the parent passes that state to a sibling component.
+const h1Style = {
+    color:      styles.color,
+    fontSize:   styles.fontSize,
+    fontFamily: styles.fontFamily,
+    padding:    styles.padding,
+    margin:     0
+};
 
-// One of the very first things that you learned about components is that they should only have one job.//
-// In the last lesson, Child had two jobs:
-// 1 - Child displayed a name.
-// 2 - Child offered a way to change that name.
-
-// You should make like Solomon and divide Child in two:
-// one component for displaying the name,
-// and a different component for allowing a user to change the name.
-
-// Notice that these lines have vanished:
-//<h1>
-//    Hey, my name is {this.props.name}!
-//</h1>
-
-export class Child extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.handleChange = this.handleChange.bind(this);
-    }
-
-    handleChange(e) {
-        const name = e.target.value;
-        this.props.onChange(name);
-    }
-
+export class AttentionGrabber extends React.Component {
     render() {
-        return (
-            <div>
-                <select
-                    id="great-names"
-                    onChange={this.handleChange}>
-
-                    <option value="Frarthur">Frarthur</option>
-                    <option value="Gromulus">Gromulus</option>
-                    <option value="Thinkpiece">Thinkpiece</option>
-                </select>
-            </div>
-        );
+        return <h1 style={h1Style}>WELCOME TO MY HOMEPAGE!</h1>;
     }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AttentionGrabber } from './child.jsx';
+import { styles } from './styles.jsx';
 
-// In React, if you write a style value as a number, then the unit "px" is assumed.
-//
-// How convenient! If you want a font size of 30px, you can write:
-//
-// { fontSize: 30 }
+// What if you want to reuse styles for several different components?
 
-const styles = {
-    background: 'lightblue',
-    color:      'darkred',
-    marginTop: 100,
-    fontSize: 50
+// One way to make styles reusable is to keep them in a separate JavaScript file.
+// This file should export the styles that you want to reuse, via export.
+// You can then import your styles into any component that wants them.
+
+const divStyle = {
+    background: styles.background,
+    height:     '100%'
 };
 
-const styleMe = <h1 style={styles}>Please style me! I am so bland!</h1>;
+export class Home extends React.Component {
+    render() {
+        return (
+            <div style={divStyle}>
+                <AttentionGrabber />
+                <footer>THANK YOU FOR VISITING MY HOMEPAGE!</footer>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
-    styleMe,
+    <Home />,
     document.getElementById('app')
 );

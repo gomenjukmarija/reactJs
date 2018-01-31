378737f2f778aefbd19268067112903a91c8d527
gomenjukmarija <gomenjukmarija@gmail.com>
2018-01-31 10:55:51 +0200

    Receive the Event Handler

--- src/child.jsx
+++ src/child.jsx
@@ -1,21 +1,39 @@
 import React from 'react';
 
-// 1) To make a child component update its parent's state,
-// the first step is something that you've seen before:
-// you must define a state-changing method on the parent.
+// If a user selects a new name from the dropdown menu,
+// then you want Child to call changeName.
+// changeName will change the parent's this.state.name,
+// which will change the name displayed in the <h1></h1>.
+//
+// Inside of Child's render function, give <select> a new attribute.
+// Make the attribute's name onChange. This will create an event listener.
+// Make the attribute's value equal to the function that you passed in from Parent.js.
 
-// Notice that the name inside of the <h1></h1> is equal to this.props.name.
-// In order to make the dropdown menu change the <h1></h1>,
-// you will need the dropdown menu to change the value of this.props.name!
 
 export class Child extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    // This new function should take an event object as an argument,
+    // extract the name that you want from that event object, and then call the event handler,
+    // passing in the extracted name!
+    // It sounds like a lot, but you will see this happen so often that it will soon feel intuitive.
+
+    handleChange(e) {
+        const name = e.target.value;
+        this.props.onChange(name);
+    }
+
     render() {
         return (
             <div>
                 <h1>
                     Hey my name is {this.props.name}!
                 </h1>
-                <select id="great-names">
+                <select id="great-names" onChange={this.handleChange}>
                     <option value="Frarthur">
                         Frarthur
                     </option>

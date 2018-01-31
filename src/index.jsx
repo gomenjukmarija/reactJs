d6acce63acdee25190c933ed004cdff4c1004628
gomenjukmarija <gomenjukmarija@gmail.com>
2018-01-31 15:57:39 +0200

    REACT FORMS. Input onChange

--- src/index.jsx
+++ src/index.jsx
@@ -1,46 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { GuineaPigs } from './child.jsx';
 
-const GUINEAPATHS = [
-    'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-1.jpg',
-    'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-2.jpg',
-    'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-3.jpg',
-    'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-4.jpg'
-];
-
-export class GuineaPigsContainer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { currentGP: 0 };
-
-        this.interval = null;
-        this.nextGP = this.nextGP.bind(this);
-    }
-
-    nextGP() {
-        let current = this.state.currentGP;
-        let next = ++current % GUINEAPATHS.length;
-        this.setState({ currentGP: next });
-
-    }
-
-    componentDidMount() {
-        this.interval = setInterval(this.nextGP, 5000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
-
+export class Input extends React.Component {
     render() {
-        console.log('this.state.currentGP',this.state.currentGP);
-        let src = GUINEAPATHS[this.state.currentGP];
-        return <GuineaPigs src={src} />;
+        return (
+            <div>
+                <input type="text" onChange={this.handleUserInput} />
+                <h1>I am an h1.</h1>
+            </div>
+        );
     }
 }
 
 ReactDOM.render(
-    <GuineaPigsContainer />,
+    <Input />,
     document.getElementById('app')
 );

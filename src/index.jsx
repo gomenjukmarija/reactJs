58aeac1480e1d377ea8df435cad4e120b0f1f85b
gomenjukmarija <gomenjukmarija@gmail.com>
2018-01-31 12:41:53 +0200

    Apply PropTypes

--- src/index.jsx
+++ src/index.jsx
@@ -1,48 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { GuineaPigs } from './child.jsx';
 
-// container component
+// Validation can ensure that your props are doing what they're supposed to be doing.
+// If props are missing, or if they're present but they aren't what you're expecting,
+// then a warning will print in the console.
+// This is useful, but reason #2 is arguably more useful: documentation.
+// Documenting props makes it easier to glance at a file and quickly understand
+// the component class inside.
+// When you have a lot of files, and you will, this can be a huge benefit.
 
-const GUINEAPATHS = [
-    'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-1.jpg',
-    'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-2.jpg',
-    'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-3.jpg',
-    'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-guineapig-4.jpg'
-];
 
-class GuineaPigsContainer extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = { currentGP: 0 };
-
-        this.interval = null;
-
-        this.nextGP = this.nextGP.bind(this);
-    }
-
-    nextGP() {
-        let current = this.state.currentGP;
-        let next = ++current % GUINEAPATHS.length;
-        this.setState({ currentGP: next });
-    }
-
-    componentDidMount() {
-        this.interval = setInterval(this.nextGP, 5000);
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
-
+export class BestSeller extends React.Component {
     render() {
-        const src = GUINEAPATHS[this.state.currentGP];
-        return <GuineaPigs src={src} />;
+        return (
+            <li>
+                Title: <span>
+          {this.props.title}
+        </span><br />
+
+                Author: <span>
+          {this.props.author}
+        </span><br />
+
+                Weeks: <span>
+          {this.props.weeksOnList}
+        </span>
+            </li>
+        );
     }
 }
 
-ReactDOM.render(
-    <GuineaPigsContainer />,
-    document.getElementById('app')
-);
+BestSeller.propTypes = {
+};

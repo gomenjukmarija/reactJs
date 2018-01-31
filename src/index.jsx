2e0b81410679e8a40e8088fb6cab7861b7190fb1
gomenjukmarija <gomenjukmarija@gmail.com>
2018-01-31 16:01:40 +0200

    Write an Input Event Handler

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export class Input extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = { userInput: '' };
+
+        this.handleUserInput = this.handleUserInput.bind(this);
+    }
+
+    handleUserInput(e) {
+        this.setState({
+            userInput: e.target.value
+        });
+    }
+
     render() {
         return (
             <div>

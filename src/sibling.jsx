2e6fe62b391f9510b094b017e06b701ac84ccd02
gomenjukmarija <gomenjukmarija@gmail.com>
2018-01-31 11:19:40 +0200

    Display Information in a Sibling Component

--- src/sibling.jsx
+++ src/sibling.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
-// Here, the name is displayed! Or at least it will be displayed, once you've done a little editing.
+// You've passed the name down to <Sibling /> as a prop.
+// Now <Sibling /> has to display that prop.
 
 export class Sibling extends React.Component {
     render() {
-
+        const name = this.props.name;
         return (
             <div>
-                <h1>Hey, my name is Frarthur!</h1>
-                <h2>Don't you think Frarthur is the prettiest name ever?</h2>
-                <h2>Sure am glad that my parents picked Frarthur!</h2>
+                <h1>Hey, my name is {name} !</h1>
+                <h2>Don't you think {name} is the prettiest name ever?</h2>
+                <h2>Sure am glad that my parents picked {name} !</h2>
             </div>
         );
     }

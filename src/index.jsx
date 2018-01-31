5d26c2026b21d10df63840966568ded2ce29b303
gomenjukmarija <gomenjukmarija@gmail.com>
2018-01-31 11:24:48 +0200

    Inline Styles

--- src/index.jsx
+++ src/index.jsx
@@ -1,36 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Child } from './child.jsx';
-import { Sibling } from './sibling.jsx';
 
-class Parent extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = { name: 'Frarthur' };
-
-        this.changeName = this.changeName.bind(this);
-    }
-
-    changeName(newName) {
-        this.setState({
-            name: newName
-        });
-    }
-
-    render() {
-        return (
-            <div>
-                <Child onChange={this.changeName} />
-                {/*That name is stored in Parent's state. Parent will have to pass the name to Sibling, so that Sibling can display it.*/}
-                {/*In Parent's render function, pass the name to <Sibling />:*/}
-                <Sibling name={this.state.name}/>
-            </div>
-        );
-    }
-}
+const styleMe = <h1 style={{ background: 'lightblue', color: 'darkred' }}>
+    Please style me! I am so bland!
+</h1>;
 
 ReactDOM.render(
-    <Parent />,
+    styleMe,
     document.getElementById('app')
 );

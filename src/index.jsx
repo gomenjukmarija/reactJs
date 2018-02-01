189547e0780cae367a88201a57b2b43342e793a9
gomenjukmarija <gomenjukmarija@gmail.com>
2018-02-01 10:00:44 +0200

    render

--- src/index.jsx
+++ src/index.jsx
@@ -1,47 +1,9 @@
-// The first mounting lifecycle method is called componentWillMount.
-// When a component renders for the first time,
-// componentWillMount gets called right before render.
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-export class Example extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = { text: '' };
-    }
-
-// 2) <Example /> calls the first mounting lifecycle method, componentWillMount.
-// 3) componentWillMount executes, and an alert appears on the screen.
-//    You can call this.setState from within componentWillMount!!!
-    componentWillMount() {
-        alert('component is about to mount!');
-        this.setState({ text: 'Hello world' });
-    }
-
-    // 4) After componentWillMount has finished,
-    // <Example /> calls the second mounting lifecycle method: render.
-    // 5) <h1>Hello world</h1> appears on the screen
-    render() {
-        return <h1>{this.state.text}</h1>;
-    }
-}
-
-//1) <Example /> is rendered for the first time.
-// <Example />'s mounting period begins.
-ReactDOM.render(
-    <Example />,
-    document.getElementById('app')
-);
-
-// 6) Two seconds later, <Example /> renders again.
-// componentWillMount does NOT get called,
-// because mounting lifecycle events only execute the first time that a component renders.
-setTimeout(() => {
-    ReactDOM.render(
-        <Example />,
-        document.getElementById('app')
-    );
-}, 2000);
-
+// render is a lifecycle method!
+// However, you should understand how render fits into the mounting period.
+// Whenever a component mounts, componentWillMount is called first,
+// followed by render, followed by componentDidMount.
+// render belongs to two categories: mounting lifecycle methods,
+// and updating lifecycle methods.
+// We'll cover updating lifecycle methods in the next lesson.
+// But first, there's one final mounting lifecycle method!

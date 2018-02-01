1846d91e455fc7f5e8a54f19f7be6c7180db57c0
gomenjukmarija <gomenjukmarija@gmail.com>
2018-02-01 11:28:44 +0200

    componentDidUpdate

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,14 @@
 /*
+The last updating lifecycle method is componentDidUpdate.
 
-The third updating lifecycle method is componentWillUpdate.
-
-componentWillUpdate gets called in between shouldComponentUpdate and render.
-
-componentWillUpdate receives two arguments: nextProps and nextState.
+When a component instance updates,
+componentDidUpdate gets called after any rendered HTML has finished loading.
 
 import React from 'react';
 
-export class Example extends React.Component {
-  componentWillUpdate(nextProps, nextState) {
-    alert('Component is about to update!  Any second now!');
+export class Example extends React.component {
+  componentDidUpdate(prevProps, prevState) {
+    alert('Component is done rendering!');
   }
 
   render() {
@@ -18,65 +16,127 @@ export class Example extends React.Component {
   }
 }
 
-You cannot call this.setState from the body of componentWillUpdate!
-Which begs the question, why would you use it?
-The main purpose of componentWillUpdate is to interact with things outside of the React architecture.
-If you need to do non-React setup before a component renders,
-such as checking the window size or interacting with an API,
-then componentWillUpdate is a good place to do that.
+componentDidUpdate automatically gets passed two arguments: prevProps and prevState.
+prevProps and prevState are references to the component's props and state before the current updating period began.
+You can compare them to the current props and state.
 
-If that sounds abstract, that's okay!
-All of the lifecycle methods might feel a bit theoretical,
-until you've used them in real-life scenarios.
-You'll be doing more of that in the next course.
+componentDidUpdate is usually used for interacting with things outside of the React environment,
+like the browser or APIs.
+It's similar to componentWillUpdate in that way, except that it gets called after render instead of before.
 */
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-const yellow = 'rgb(255, 215, 18)';
+import { TopNumber } from './TopNumber';
+import { Display } from './Display';
+import { Target } from './Target';
+import { random, clone } from './helpers';
 
-export class TopNumber extends React.Component {
+const fieldStyle = {
+    position: 'absolute',
+    width: 250,
+    bottom: 60,
+    left: 10,
+    height: '60%',
+};
+
+class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { 'highest': 0 };
+        this.state = {
+            game: false,
+            targets: {},
+            latestClick: 0
+        };
+
+        this.intervals = null;
+
+        this.hitTarget = this.hitTarget.bind(this);
+        this.startGame = this.startGame.bind(this);
+        this.endGame = this.endGame.bind(this);
     }
 
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.number > this.state.highest) {
-            this.setState({
-                highest: nextProps.number
-            });
+    // You want the game to end when a user clicks a number less than their previous click. componentDidUpdate can do that!
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.latestClick < prevState.latestClick) {
+            this.endGame();
         }
     }
 
-    // On every render, you want componentWillUpdate to check whether the background is yellow already.
-    // If it isn't, then you want to check whether the top number is at least 950,000.
-    // If it is, then make the background yellow.
-    // Of course, you'll also need to change the color back to white when a user starts a new game!
-    // You can do this with the help of this.props.game,
-    // a boolean that reports whether or not a current game is in progress.
+    createTarget(key, ms) {
+        ms = ms || random(500, 2000);
+        this.intervals.push(setInterval(function(){
+            let targets = clone(this.state.targets);
+            let num = random(1, 1000*1000);
+            targets[key] = targets[key] != 0 ? 0 : num;
+            this.setState({ targets: targets });
+        }.bind(this), ms));
+    }
 
-    componentWillUpdate(nextProps, nextState) {
-        if (document.body.style.background != yellow
-            && this.state.highest >= 850*100) {
-            document.body.style.background = yellow;
-        } else if (!this.props.game
-            && nextProps.game) {
-            document.body.style.background = 'white';
+    hitTarget(e) {
+        if (e.target.className != 'target') return;
+        let num = parseInt(e.target.innerText);
+        for (let target in this.state.targets) {
+            let key = Math.random().toFixed(4);
+            this.createTarget(key);
         }
+        this.setState({ latestClick: num });
+    }
+
+    startGame() {
+        this.createTarget('first', 750);
+        this.setState({
+            game: true
+        });
+    }
+
+    endGame() {
+        this.intervals.forEach((int) => {
+            clearInterval(int);
+        });
+        this.intervals = [];
+        this.setState({
+            game: false,
+            targets: {},
+            latestClick: 0
+        });
+    }
+
+    componentWillMount() {
+        this.intervals = [];
     }
 
     render() {
+        let buttonStyle = {
+            display: this.state.game ? 'none' : 'inline-block'
+        };
+        let targets = [];
+        for (let key in this.state.targets) {
+            targets.push(
+                <Target
+                    number={this.state.targets[key]}
+                    key={key} />
+            );
+        }
         return (
-            <h1>
-                Top Number: {this.state.highest}
-            </h1>
+            <div>
+                <TopNumber number={this.state.latestClick} game={this.state.game} />
+                <Display number={this.state.latestClick} />
+                <button onClick={this.startGame} style={buttonStyle}>
+                    New Game
+                </button>
+                <div style={fieldStyle} onClick={this.hitTarget}>
+                    {targets}
+                </div>
+            </div>
         );
     }
 }
 
-TopNumber.propTypes = {
-    number: React.PropTypes.number,
-    game: React.PropTypes.bool
-};
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
+
